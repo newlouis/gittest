d750d85cdf5f4481738f0871bfeea1465a6f6457
louis <junsm2000@163.com>
2017-02-28 14:51:25 +0800

    初始化项目代码

---
+++ gittest/src/TestApp.java
@@ -0,0 +1,21 @@
+/**
+ * <ul>
+ * <li> <b>目的:</b> <br />
+ * <p>
+ * </p>
+ * </li>
+ * <li><b>注意事项：</b></li>
+ * <li><b>修改历史：</b><br />
+ * <p>
+ * 创建: 2017-2-28 下午2:46:38<br />
+ * 作者:lujun
+ * </p>
+ * </li>
+ * <li><b>已知问题：</b></li>
+ * </ul>
+ */
+public class TestApp {
+	public void main(String[] args){
+		System.out.println("testApp,first git servlet");
+	}
+}
